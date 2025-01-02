8b5690afb41a526460f2cadbe122f9cb6f7cf4b3
underthemo-onlight <underthemo_onlight@naver.com>
2025-01-02 11:19:46 +0900

    ✨ react로 작성될 수 있도록 구조 업데이트

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "@components/layout/Header";
-import Footer from "@components/layout/Footer";
+import NavigationBar from "@components/layout/NavigationBar";
 
 export default function Layout() {
   return (
@@ -9,7 +9,7 @@ export default function Layout() {
 
       <Outlet />
 
-      <Footer />
+      <NavigationBar />
     </div>
   );
 }
